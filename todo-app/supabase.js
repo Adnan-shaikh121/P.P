a6e92540057805c6c3a4f76712e98ee3e1212e8c
Adnan_shaikh <107258786+Adnan-shaikh121@users.noreply.github.com>
2023-08-22 20:09:15 +0530

    delet file

--- todo-app/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://bnyfcftmqqiqrwyrgpyy.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJueWZjZnRtcXFpcXJ3eXJncHl5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ0MDMzNjUsImV4cCI6MTk5OTk3OTM2NX0.0J_WAVgHLczCh1cyXfcFNv2_NPrG5_OmbUtEyBECPmc';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
