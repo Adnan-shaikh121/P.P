eb797f443b78f02bb4dcdc3777c915251e58d9c3
Adnan_shaikh <107258786+Adnan-shaikh121@users.noreply.github.com>
2023-08-24 16:33:42 +0530

    3rd commit

--- tesla-clone/src/components/Header.js
+++ tesla-clone/src/components/Header.js
@@ -1,10 +1,13 @@
 import React from "react";
+import styled from "styled-components"
 
 function Header(){
     return(
-        <div>
-            
-        </div>
+        <Container>
+            Header
+        </Container>
     )
 }
 export default Header;
+
+const Container = styled.div``
