23096204cc4a4eb93a9e5f32809ff1b5af6086ca
Adnan_shaikh <107258786+Adnan-shaikh121@users.noreply.github.com>
2023-08-24 15:37:42 +0530

    5th commit

--- tesla-clone/src/components/Home.js
+++ tesla-clone/src/components/Home.js
@@ -6,26 +6,35 @@ function Home(){
     return(
         <Container>
             <Section title="Model S"
-            backgroundImg="model-s"
+            backgroundImg="model-s.jpg"
             description="Order online for Touchness Delivery"
             leftButoon="Custom Order"
             rightOrder="Existing Inventor"/>
             <Section title="Model Y"
-                backgroundImg="model-y"
+                backgroundImg="model-y.jpg"
                 description="Order online for Touchness Delivery"
                 leftButoon="Custom Order"
                 rightOrder="Existing Inventor"/>
             <Section title="Model 3"
-                backgroundImg="model-3"
+                backgroundImg="model-3.jpg"
                 description="Order online for Touchness Delivery"
                 leftButoon="Custom Order"
                 rightOrder="Existing Inventor" />
             <Section title="Model X"
-                backgroundImg="model-x"
+                backgroundImg="model-x.jpg"
                 description="Order online for Touchness Delivery"
                 leftButoon="Custom Order"
                 rightOrder="Existing Inventor" />
-            <Section/>
+            <Section title="Solar Panels"
+                backgroundImg="solar-panel.jpg"
+                description="Order online for Touchness Delivery"
+                leftButoon="Custom Order"
+                rightOrder="Existing Inventor"/>
+            <Section title="Solar Roof"
+                backgroundImg="solar-roof.jpg"
+                description="Order online for Touchness Delivery"
+                leftButoon="Custom Order"
+                rightOrder="Existing Inventor" />
         </Container>
     )
 }
